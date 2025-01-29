c84d4f103191bdd65d444fbc9c604e9e406812ac
Pavan1102701 <pavansrisaiarava15@gmail.com>
2025-01-29 12:24:15 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿class Program1
+{
+    public int x;
+    public int y;
+}
+
+class Program
+{
+    static void Main()
+    {
+
+          Program1 p  = new Program1();
+        p.x = 10;
+        p.y = 15;
+        Console.WriteLine($"x = {p.x}, y = {p.y}");
+    }
+}
